1dd3fefc14988e44879c5a540aa04b5083fb30fd
RedJocker85 <mauricio.guaruja@gmail.com>
2021-04-27 15:30:29 -0300

    initial commit

---
+++ src/test/java/org/tutorial/dio/cloudparking/CloudParkingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tutorial.dio.cloudparking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudParkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
